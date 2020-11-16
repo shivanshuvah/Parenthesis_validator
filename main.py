dc19717788d9cde9e06b4badce26503035d3b51d
unknown <shivvah.misc@gmail.com>
2020-11-16 16:49:39 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+#F (2) = a[x] + {2 + 3}
+#true
+#(A +
+##false
+#{( a + b } )
+#false
+
+#parse the string for brackets 
+#implement stack in list
+#append for open parenthesis
+#pop for close parenthesis
+#check if they form a pair
+
+
+
+
+def parenthesis_validator(input_string):
+    brackets=[]
+    bracket_map={'{':'}','(':')','[':']'}
+    for char in input_string:
+        if char in ['(','{','[']:
+            brackets.append(char)
+        elif char in [')','}',']']:
+            try: 
+                last_element=brackets.pop() #(, char=)
+                if bracket_map.get(last_element) == char:
+                    continue
+                else :
+                    return False
+            except IndexError:
+                return False
+    if brackets:       
+        return False # if everything is validated
+    else:
+        return True
+
+input_string=input()
+print(parenthesis_validator(input_string))
+
+
+
+
+
+
